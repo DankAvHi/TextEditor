8142b706aa25f0077a95d0b874d1c19ff93dedf5
DankAvHi <thexcat86@gmail.com>
2023-05-11 20:39:47 +0300

    [fix] quotes rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   extends: "@react-native-community",
   rules: {
     "prettier/prettier": 0,
-    quotes: ["error", "double"],
+    quotes: 0,
     "react/react-in-jsx-scope": 0,
   },
 };
