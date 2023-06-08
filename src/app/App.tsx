935d0d596488039f29091380cef2bbb2f5625c65
DankAvHi <thexcat86@gmail.com>
2023-06-08 11:12:23 +0300

    [add] wrap all in appProvider; use TextField comp instead of hardcode

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,22 @@
-import {
-  SafeAreaView,
-  StatusBar,
-  TextInput,
-  useColorScheme,
-} from "react-native";
+import { SafeAreaView, StatusBar } from "react-native";
 import { useAppStyles } from "./App.styles";
 import { AppTopBar } from "../widgets";
+import { AppProvider } from "./providers/AppProvider";
+import { TextField } from "../widgets";
 
 export const App = (): JSX.Element => {
   const { styles, colorScheme } = useAppStyles();
 
   return (
-    <SafeAreaView style={styles.App}>
-      <StatusBar
-        barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
-        backgroundColor={colorScheme === "dark" ? "black" : "white"}
-      />
-      <AppTopBar />
-      <TextInput
-        style={{ padding: 12, fontSize: 18, lineHeight: 1.5 }}
-        multiline
-        placeholder="Enter your text here..."
-        placeholderTextColor={"gray"}
-      />
-    </SafeAreaView>
+    <AppProvider>
+      <SafeAreaView style={styles.App}>
+        <StatusBar
+          barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
+          backgroundColor={colorScheme === "dark" ? "black" : "white"}
+        />
+        <AppTopBar />
+        <TextField />
+      </SafeAreaView>
+    </AppProvider>
   );
 };
