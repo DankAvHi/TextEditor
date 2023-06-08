8916423f02bd48de86804e2138f5bf37a3a17104
DankAvHi <thexcat86@gmail.com>
2023-06-08 11:11:39 +0300

    [add] indicator if editable file is has changes

--- src/widgets/AppTopBar/AppTopBar.tsx
+++ src/widgets/AppTopBar/AppTopBar.tsx
@@ -1,11 +1,16 @@
 import { Text, View } from "react-native";
 import { useAppTopBarStyles } from "./AppTopBar.styles";
+import { useAppContext } from "../../app/providers/AppProvider";
 
 export const AppTopBar = () => {
+  const { isFileHaveChanges } = useAppContext();
+
   const { styles } = useAppTopBarStyles();
   return (
     <View style={styles.AppTopBar}>
-      <Text style={styles.fileName}>{`Untitled`}</Text>
+      <Text style={styles.fileName}>{`Untitled ${
+        isFileHaveChanges ? `*` : ``
+      }`}</Text>
     </View>
   );
 };
