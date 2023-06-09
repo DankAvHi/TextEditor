f23618cb2a813fd3c0aa8bd6b4e50aead408f0f2
DankAvHi <thexcat86@gmail.com>
2023-06-09 14:13:13 +0300

    [add] is file changed logic

--- src/widgets/TextField/TextField.tsx
+++ src/widgets/TextField/TextField.tsx
@@ -1,14 +1,19 @@
 import { createRef } from "react";
 import { TextInput } from "react-native";
 import { useTextFieldStyles } from "./TextField.styles";
+import { useAppContext } from "../../app/providers/AppProvider";
 
 export const TextField = () => {
   const { styles } = useTextFieldStyles();
+  const { setIsFileHaveChanges } = useAppContext();
 
   const textInputRef = createRef<TextInput>();
 
+  const textInputOnChange = () => setIsFileHaveChanges((_prev) => true);
+
   return (
     <TextInput
+      onChange={textInputOnChange}
       ref={textInputRef}
       style={styles.TextField}
       multiline
