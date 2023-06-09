9d0dafd27ac2c43960862d1df81b77c77e9dfb91
DankAvHi <thexcat86@gmail.com>
2023-06-09 14:10:00 +0300

    [add] svg transformer module to config

--- metro.config.js
+++ metro.config.js
@@ -6,9 +6,20 @@
  */
 const { makeMetroConfig } = require("@rnx-kit/metro-config");
 const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
+
+const defaultAssetExts =
+  require("metro-config/src/defaults/defaults").assetExts;
+const defaultSourceExts =
+  require("metro-config/src/defaults/defaults").sourceExts;
+
 module.exports = makeMetroConfig({
   projectRoot: __dirname,
   resolver: {
     resolveRequest: MetroSymlinksResolver(),
+    assetExts: defaultAssetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...defaultSourceExts, "svg"],
+  },
+  transformer: {
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
   },
 });
